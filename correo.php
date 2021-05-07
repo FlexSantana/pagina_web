6414edebfdcc91185876ae0e4e79a4c4abce7f0f
FlexSantana <76594081+FlexSantana@users.noreply.github.com>
2021-05-07 13:37:54 -0400

    Add files via upload

--- correo.php
+++ correo.php
@@ -1,16 +1,17 @@
 <?php 
 
 if(isset($_POST['enviar'])) {
-    if(!empty($_POST['name']) && !empty($_POST['email'] && !empty($_POST['subject'] && !empty($_POST['messsge'])
+    if(!empty($_POST['name']) && !empty($_POST['email'] && !empty($_POST['subject'] && !empty($_POST['message'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['message'];
-        $header = "From: $email" . "\r\n";
+        $header = "From: noreply@example.com" . "\r\n";
         $header.= "Reply-To: flexsantana99@gmail.com" . "\r\n";
         $header.= "X-Mailer: PHP/". phpversion();
         $mail = mail($email, $subject, $message, $header);
-        if($mail){
+        if($mail) {
             echo "<h4>Enviado</h4>";
         }
+    }
 }
